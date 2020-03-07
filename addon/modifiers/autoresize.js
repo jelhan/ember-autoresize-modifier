e6b1cab8a85b61d895ec31695d2b2be7036e9ec2
Jeldrik Hanschke <admin@jhanschke.de>
2020-03-07 17:03:04 +0100

    support box-sizing: border-box

--- addon/modifiers/autoresize.js
+++ addon/modifiers/autoresize.js
@@ -1,8 +1,19 @@
 import { modifier } from 'ember-modifier';
 
 function resize(element) {
+  // height must be calculated independently from height previously enforced
   element.style.height = 'auto';
-  element.style.height = `${element.scrollHeight}px`;
+
+  let isBorderBox = window.getComputedStyle(element).boxSizing === 'border-box';
+  let requiredHeight = element.scrollHeight;
+
+  if (isBorderBox) {
+    // borders must be added on top of scrollHeight if box-sizing is border-box
+    let borderHeight = element.offsetHeight - element.clientHeight;
+    requiredHeight += borderHeight;
+  }
+
+  element.style.height = `${requiredHeight}px`;
 }
 
 function eventHandler(event) {
