9b8df1d4bc03a8ec97d6298e5178d6560fa38400
Jeldrik Hanschke <admin@jhanschke.de>
2023-01-07 12:02:04 +0100

    reset changes to modifier

--- addon/modifiers/autoresize.js
+++ addon/modifiers/autoresize.js
@@ -24,16 +24,12 @@ export default class AutoresizeModifier extends Modifier {
     }
 
     let capitalizeDimension = capitalize(dimension);
-    let computedStyle = window.getComputedStyle(element);
-    let minDimension = computedStyle[`min${capitalizeDimension}`];
 
     // height / width must be calculated independently from height / width previously enforced
-    // and calculation should start from min height or width when specified
-    element.style[dimension] = parseInt(minDimension, 10)
-      ? minDimension
-      : 'auto';
+    element.style[dimension] = 'auto';
 
-    let isBorderBox = computedStyle.boxSizing === 'border-box';
+    let isBorderBox =
+      window.getComputedStyle(element).boxSizing === 'border-box';
     let requiredDimension = element[`scroll${capitalizeDimension}`];
 
     if (isBorderBox) {
